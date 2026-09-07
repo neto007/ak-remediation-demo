8b7e95e87147793eda8222c167ce384f1ccc87f2
Salvador Neto <salvador.dilio@gmail.com>
2026-09-07 13:32:01 -0300

    fix(deps): bump lodash to 4.17.21 for CVE-2019-10744

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const _ = require('lodash');
 
-// Extrai o nome de cada usuário. `_.pluck` foi removido no lodash 4;
-// o equivalente é `_.map` com o caminho da propriedade.
 function names(users) {
   return _.map(users, 'name');
 }
